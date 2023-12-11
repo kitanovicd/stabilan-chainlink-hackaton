8efbcc5d182f998dac949317e812a983cd73ddcb
Srdjan Rakic <bans.cowboy@gmail.com>
2023-12-11 10:03:58 +0100

    fixed button go to dashboard

--- frontend/src/app/banner/BannerWrapper.tsx
+++ frontend/src/app/banner/BannerWrapper.tsx
@@ -30,7 +30,7 @@ export const BannerWrapper = () => {
       action={
         <Link
           href={RouterSettings.admin}
-          className="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="px-5 py-2 me-2 text-xs font-medium text-white bg-success rounded-3xl hover:bg-success-hover transition-colors duration-500"
         >
           Go To Dashboard
         </Link>
